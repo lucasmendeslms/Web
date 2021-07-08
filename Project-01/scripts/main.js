90897521eb2b6111c2c83ad73959fdec9069e15a
Lucas Mendes <57737521+lucasmendeslms@users.noreply.github.com>
2021-07-08 17:27:36 -0300

    First Commit

--- Project-01/scripts/main.js
+++ Project-01/scripts/main.js
@@ -1,6 +1,3 @@
 const meuCabecalho = document.querySelector('h1');
 meuCabecalho.textContent = 'Vamos Viajar?';
 
-document.querySelector('html').onclick = function() {
-    alert('Ai! Pare de me cutucar!');
-}
